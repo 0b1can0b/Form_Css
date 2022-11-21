23e8af5061ce862255655cf6fb1a46ca563a215e
0b1can0b <0b1can0b21@gmail.com>
2022-11-21 15:48:51 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import { useState, useEffect } from "react";
 import "./App.scss";
 
+import * as IconModule from "react-icons/sl";
+
 console.clear();
+
+console.log(IconModule);
+
+const iconsList = Object.keys(IconModule)
+  .map((e) => {
+    return { icon: e, rand: Math.random() };
+  })
+  .sort((a, b) => a.rand - b.rand)
+  .map((e) => e.icon)
+  .splice(0, 10);
+
+console.log(iconsList);
 
 const App = () => {
   return (
     <div className="app">
-      <div className="custom_logo_icon">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+      {iconsList.map((e, i) => {
+        const TempCompnent = IconModule[e];
+        return <TempCompnent key={i} />;
+      })}
     </div>
   );
 };
